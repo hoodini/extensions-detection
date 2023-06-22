3d24316d6024cf645ede7aff8932f211077ffedc
Yuval Avidani <48050809+hoodini@users.noreply.github.com>
2023-06-22 13:26:00 +0300

    Update extensions.py to add version number

--- extensions.py
+++ extensions.py
@@ -4,14 +4,16 @@ import collections
 import csv
 from tabulate import tabulate
 
-def get_extension_name(extension_folder):
+def get_extension_name_and_version(extension_folder):
     manifest_file = os.path.join(extension_folder, 'manifest.json')
     try:
         with open(manifest_file, 'r') as file:
             data = json.load(file)
-            return data.get('name', 'Name not found in manifest.json')
+            name = data.get('name', 'Name not found in manifest.json')
+            version = data.get('version', 'Version not found in manifest.json')
+            return name, version
     except:
-        return 'Error reading manifest.json'
+        return 'Error reading manifest.json', ''
 
 def get_browser_extensions(browser):
     if browser.lower() == 'chrome':
@@ -29,10 +31,10 @@ def get_browser_extensions(browser):
         extension_path = os.path.join(path, folder_name)
         latest_version = sorted(os.listdir(extension_path))[-1]
         extension_folder = os.path.join(extension_path, latest_version)
-        extension_name = get_extension_name(extension_folder)
+        extension_name, extension_version = get_extension_name_and_version(extension_folder)
         if "__MSG_" in extension_name:
             extension_name = "Localization placeholder detected. Human-readable name not available."
-        extensions[folder_name] = extension_name
+        extensions[folder_name] = (extension_name, extension_version)
 
     return extensions
 
@@ -42,23 +44,23 @@ extension_data = collections.defaultdict(list)
 
 for browser in browsers:
     extensions = get_browser_extensions(browser)
-    for extension, name in extensions.items():
-        extension_data[name].append(browser)
+    for extension, (name, version) in extensions.items():
+        extension_data[(name, version)].append(browser)
 
 # Filter out entries with errors or localization placeholders
 filtered_extension_data = {
-    name: browsers 
-    for name, browsers in extension_data.items() 
+    (name, version): browsers 
+    for (name, version), browsers in extension_data.items() 
     if name != "Error reading manifest.json" and 
        name != "Localization placeholder detected. Human-readable name not available."
 }
 
 with open('extensions.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Extension', 'Browser'])
-    for extension, browsers in filtered_extension_data.items():
-        writer.writerow([extension, ', '.join(browsers)])
+    writer.writerow(['Extension', 'Version', 'Browser'])
+    for (extension, version), browsers in filtered_extension_data.items():
+        writer.writerow([extension, version, ', '.join(browsers)])
 
 # Tabulate data for console output
-table_data = [[extension, ', '.join(browsers)] for extension, browsers in filtered_extension_data.items()]
-print(tabulate(table_data, headers=['Extension', 'Browser']))
+table_data = [[extension, version, ', '.join(browsers)] for (extension, version), browsers in filtered_extension_data.items()]
+print(tabulate(table_data, headers=['Extension', 'Version', 'Browser']))
