3fd9951db535d725c534e18bbd02c3fcbc1b8e95
Yuval Avidani <48050809+hoodini@users.noreply.github.com>
2023-06-22 15:19:16 +0300

    Update extensions.py - fixed hebrew and added fields

--- extensions.py
+++ extensions.py
@@ -2,18 +2,37 @@ import os
 import json
 import collections
 import csv
+import socket
 from tabulate import tabulate
+
+def get_localized_string(extension_folder, placeholder):
+    key = placeholder[6:-2]
+    locale_file = os.path.join(extension_folder, '_locales', 'en', 'messages.json')
+    try:
+        with open(locale_file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data.get(key, {}).get('message', 'Localized string not found')
+    except Exception as e:
+        print(f"Error reading localization file: {e}")
+        return 'Error reading localization file'
 
 def get_extension_name_and_version(extension_folder):
     manifest_file = os.path.join(extension_folder, 'manifest.json')
     try:
-        with open(manifest_file, 'r') as file:
+        with open(manifest_file, 'r', encoding='utf-8') as file:
             data = json.load(file)
-            name = data.get('name', 'Name not found in manifest.json')
+            name = data.get('name', 'Localization placeholder detected. Human-readable name not available.')
             version = data.get('version', 'Version not found in manifest.json')
-            return name, version
-    except:
-        return 'Error reading manifest.json', ''
+            author = data.get('author', 'Author not found in manifest.json')
+            default_title = data.get('browser_action', {}).get('default_title', 'Title not found in manifest.json')
+            if "__MSG_" in name:
+                name = get_localized_string(extension_folder, name)
+            if "__MSG_" in default_title:
+                default_title = get_localized_string(extension_folder, default_title)
+            return name, version, author, default_title
+    except Exception as e:
+        print(f"Error reading manifest.json: {e}")
+        return 'Error reading manifest.json', '', '', ''
 
 def get_browser_extensions(browser):
     if browser.lower() == 'chrome':
@@ -21,20 +40,18 @@ def get_browser_extensions(browser):
     elif browser.lower() == 'edge':
         path = os.path.expanduser('~') + "\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Extensions\\"
     else:
-        return []
+        return {}
 
     if not os.path.exists(path):
-        return []
+        return {}
 
     extensions = {}
     for folder_name in os.listdir(path):
         extension_path = os.path.join(path, folder_name)
         latest_version = sorted(os.listdir(extension_path))[-1]
         extension_folder = os.path.join(extension_path, latest_version)
-        extension_name, extension_version = get_extension_name_and_version(extension_folder)
-        if "__MSG_" in extension_name:
-            extension_name = "Localization placeholder detected. Human-readable name not available."
-        extensions[folder_name] = (extension_name, extension_version)
+        extension_name, extension_version, author, default_title = get_extension_name_and_version(extension_folder)
+        extensions[folder_name] = [extension_name, extension_version, author, default_title]
 
     return extensions
 
@@ -44,23 +61,20 @@ extension_data = collections.defaultdict(list)
 
 for browser in browsers:
     extensions = get_browser_extensions(browser)
-    for extension, (name, version) in extensions.items():
-        extension_data[(name, version)].append(browser)
+    for extension_id, details in extensions.items():
+        extension_data[extension_id].append(details + [browser])
 
-# Filter out entries with errors or localization placeholders
-filtered_extension_data = {
-    (name, version): browsers 
-    for (name, version), browsers in extension_data.items() 
-    if name != "Error reading manifest.json" and 
-       name != "Localization placeholder detected. Human-readable name not available."
-}
+username = os.getlogin()
+hostname = socket.gethostname()
 
 with open('extensions.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Extension', 'Version', 'Browser'])
-    for (extension, version), browsers in filtered_extension_data.items():
-        writer.writerow([extension, version, ', '.join(browsers)])
+    writer.writerow(['Username', 'Hostname', 'Extension ID', 'Extension', 'Version', 'Author', 'Title', 'Browser'])
+    for extension_id, details_list in extension_data.items():
+        for details in details_list:
+            extension_name, extension_version, author, default_title, browser = details
+            writer.writerow([username, hostname, extension_id, extension_name, extension_version, author, default_title, browser])
 
 # Tabulate data for console output
-table_data = [[extension, version, ', '.join(browsers)] for (extension, version), browsers in filtered_extension_data.items()]
-print(tabulate(table_data, headers=['Extension', 'Version', 'Browser']))
+table_data = [[username, hostname, extension_id, extension_name, version, author, default_title, browser] for extension_id, details_list in extension_data.items() for extension_name, version, author, default_title, browser in details_list]
+print(tabulate(table_data, headers=['Username', 'Hostname', 'Extension ID', 'Extension', 'Version', 'Author', 'Title', 'Browser']))
